cb5c861ec9c53797d9651e9a5e702d7151029082
xeparc <xeparc@gmail.com>
2024-08-23 19:57:59 +0300

    - Added builder for ChromaNet - Changes in Dataloader builder - Added builder for Loss Criterion - Added builder for Optimizer - Added builder for LR Scheduler - Added builder for logging

--- builders.py
+++ builders.py
@@ -1,4 +1,6 @@
 import json
+import logging
+import os
 
 import torch
 import torch.utils
@@ -6,16 +8,18 @@ import torch.utils.data
 
 from dataset import (
     DatasetQuantizedJPEG,
-    ToDCTTensor
+    ToDCTTensor,
+    ToQTTensor
 )
 from models import (
     SpectralNet,
     SpectralEncoder,
+    ChromaNet,
+    ConvNeXtBlock,
     InverseDCT,
-    ChromeUpsample,
-    ChromaCrop,
     ConvertYccToRGB
 )
+from utils import CombinedLoss
 
 
 def get_dct_stats(config):
@@ -78,7 +82,35 @@ def build_spectral_net(config):
 
 
 def build_chroma_net(config):
-    pass
+    
+    depths =    config.MODEL.CHROMA.DEPTHS
+    channels =  config.MODEL.CHROMA.CHANNELS
+
+    num_blocks = len(depths)
+    body = []
+    for i in range(num_blocks):
+        for _ in range(depths[i]):
+            block = ConvNeXtBlock(
+                in_channels=    channels[i],
+                mid_channels=   config.MODEL.CHROMA.CHANNEL_MULTIPLIER * channels[i],
+                kernel_size=    config.MODEL.CHROMA.BODY_KERNEL_SIZE
+            )
+            body.append(block)
+    
+    if config.MODEL.RGB_OUTPUT:
+        out_transform = ConvertYccToRGB
+    else:
+        out_transform = torch.nn.Identity
+
+    net = ChromaNet(
+        stages=             body,
+        output_transform=   out_transform,
+        in_channels=        config.MODEL.CHROMA.IN_CHANNELS,
+        out_channels=       config.MODEL.CHROMA.OUT_CHANNELS,
+        kernel_size=        config.MODEL.CHROMA.STEM_KERNEL_SIZE,
+    )
+
+    return net
 
 
 def build_dataloader(config, kind: str):
@@ -86,14 +118,16 @@ def build_dataloader(config, kind: str):
 
     if kind == "train":
         image_dirs = config.DATA.LOCATIONS.TRAIN
-        batch_size = config.TRAIN.BATCH_SIZE // config.DATA.NUM_PATCHES
+        num_patches = config.DATA.NUM_PATCHES
+        batch_size = config.TRAIN.BATCH_SIZE // num_patches
     elif kind == "val":
         image_dirs = config.DATA.LOCATIONS.VAL
-        batch_size = config.VALIDATION.BATCH_SIZE // config.DATA.NUM_PATCHES
+        num_patches = 1
+        batch_size = config.VALIDATION.BATCH_SIZE
     elif kind == "test":
         image_dirs = config.DATA.LOCATIONS.TEST
-        batch_size = config.TEST.BATCH_SIZE // config.DATA.NUM_PATCHES
-
+        num_patches = 1
+        batch_size = config.TEST.BATCH_SIZE 
 
     if config.DATA.NORMALIZE_DCT:
         coeffs = get_dct_stats(config)
@@ -101,22 +135,25 @@ def build_dataloader(config, kind: str):
     else:
         transform_dct = ToDCTTensor()
 
+    transform_qt = ToQTTensor(config.DATA.INVERT_QT)
+
     dataset = DatasetQuantizedJPEG(
         image_dirs=         image_dirs,
         patch_size=         config.DATA.PATCH_SIZE,
-        num_patches=        config.DATA.NUM_PATCHES,
+        num_patches=        num_patches,
         min_quality=        config.DATA.MIN_QUALITY,
         max_quality=        config.DATA.MAX_QUALITY,
         target_quality=     config.DATA.TARGET_QUALITY,
         subsample=          config.DATA.SUBSAMPLE,
         transform_dct=      transform_dct,
+        transform_qt=       transform_qt,
         use_lq_rgb=         config.DATA.USE_LQ_RGB,
         use_lq_ycc=         config.DATA.USE_LQ_YCC,
         use_lq_dct=         config.DATA.USE_LQ_DCT,
         use_hq_rgb=         config.DATA.USE_HQ_RGB,
         use_hq_ycc=         config.DATA.USE_HQ_YCC,
         use_hq_dct=         config.DATA.USE_HQ_DCT,
-        use_qtables=        config.DATA.USE_QTABLES,
+        use_qt=             config.DATA.USE_QTABLES,
         seed=               config.SEED
     )
 
@@ -131,3 +168,91 @@ def build_dataloader(config, kind: str):
     )
 
     return dataloader
+
+
+def build_criterion(config):
+    kwargs = {k:v for k,v in config.LOSS.CRITERION_KWARGS}
+    if config.LOSS.CRITERION == "huber":
+        criterion = torch.nn.HuberLoss(**kwargs)
+    elif config.LOSS.CRITERION == "mse":
+        criterion = torch.nn.MSELoss(**kwargs)
+    else:
+        raise ValueError
+
+    return CombinedLoss(
+        criterion=      criterion,
+        gamma=          config.LOSS.GAMMA,
+        luma_weight=    config.LOSS.LUMA_WEIGHT,
+        chroma_weight=  config.LOSS.CHROMA_WEIGHT
+    )
+
+
+def build_optimizer(config, spectral_params, chroma_params):
+
+    name = config.TRAIN.OPTIMIZER.NAME
+    kwargs = {k:v for k,v in config.TRAIN.OPTIMIZER.KWARGS}
+
+    if config.TRAIN.LR_SCHEDULER.WARMUP_PREFIX:
+        lr = config.TRAIN.WARMUP_LR
+    else:
+        lr = config.TRAIN.BASE_LR
+
+    if name == "adamw":
+        optim = torch.optim.AdamW(
+            params=[{"params": spectral_params, "params": chroma_params}],
+            lr=lr,
+            **kwargs
+        )
+    return optim
+
+
+def build_lr_scheduler(config, optimizer):
+
+    kwargs = {k:v for k,v in config.TRAIN.LR_SCHEDULER.KWARGS}
+    if config.TRAIN.LR_SCHEDULER.NAME == "cosine":
+        main_scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(
+            optimizer,
+            **kwargs
+        )
+    elif config.TRAIN.LR_SCHEDULER_NAME == "multistep":
+        main_scheduler = torch.optim.lr_scheduler.MultiStepLR(
+            optimizer,
+            **kwargs
+        )
+    elif config.TRAIN_LR_SCHEDULER_NAME == "plateau":
+        main_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer,
+            **kwargs
+        )
+    else:
+        raise ValueError
+
+    if config.TRAIN.LR_SCHEDULER.WARMUP_PREFIX:
+        warmup_scheduler = torch.optim.lr_scheduler.ConstantLR(
+            optimizer,
+            factor=1.0,
+            total_iters=config.TRAIN.WARMUP_ITERATIONS
+        )
+        return torch.optim.lr_scheduler.ChainedScheduler([
+            warmup_scheduler, main_scheduler
+        ])
+    else:
+        return main_scheduler
+
+
+def build_logger(config, name="train"):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    logger.propagate = False
+
+    # create formatter
+    fmt = '[%(asctime)s %(name)s] (%(filename)s %(lineno)d): %(levelname)s %(message)s'
+
+    # create file handlers
+    savepath = os.path.join(config.LOGGING.DIR, "log.txt")
+    file_handler = logging.FileHandler(savepath, mode='a')
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(logging.Formatter(fmt=fmt, datefmt='%Y-%m-%d %H:%M:%S'))
+    logger.addHandler(file_handler)
+
+    return logger
--- config.py
+++ config.py
@@ -7,6 +7,7 @@ _C = CN()
 _C.BASE = ['']
 
 _C.SEED = 7
+_C.TAG = "default"
 
 # -----------------------------------------------------------------------------
 # Datatest config
@@ -28,16 +29,17 @@ _C.DATA.USE_LQ_RGB = False
 _C.DATA.USE_LQ_YCC = False
 _C.DATA.USE_LQ_DCT = True
 # Include target image's RGB channels in datapoints ?
-_C.DATA.USE_HQ_RGB = False
+_C.DATA.USE_HQ_RGB = True
 # Include target image's YCbCr planes in datapoints ?
 _C.DATA.USE_HQ_YCC = True
 # Include target image's DCT in datapoints ?
-_C.DATA.USE_HQ_DCT = True
+_C.DATA.USE_HQ_DCT = False
 # Include JPEG quantization tables in datapoints ?
 _C.DATA.USE_QTABLES = True
 _C.DATA.NORMALIZE_RGB = False
 _C.DATA.NORMALIZE_YCC = False
 _C.DATA.NORMALIZE_DCT = True
+_C.DATA.INVERT_QT = False
 _C.DATA.DCT_STATS_FILEPATH = "data/DIV2K+Flickr2K-dct-stats.json"
 _C.DATA.PIN_MEMORY = True
 _C.DATA.PIN_MEMORY_DEVICE = "mps"
@@ -50,6 +52,7 @@ _C.DATA.NUM_WORKERS = 1
 # -----------------------------------------------------------------------------
 _C.MODEL = CN()
 _C.MODEL.NAME = ""
+_C.MODEL.RGB_OUTPUT = True
 
 # Localized Frequency Domain Transformer (LFDT) parameters
 # We'll call it Spectral Transformer :)
@@ -69,15 +72,44 @@ _C.MODEL.SPECTRAL.DROPOUTS = [0.1, 0.1, 0.1]
 # Chrominance Upscale Net
 # We'll call it Chroma net
 _C.MODEL.CHROMA = CN()
-_C.MODEL.CHROMA.DEPTHS = [1, 1, 1]
-_C.MODEL.CHROMA.CHANNELS = [32, 64, 3]
+_C.MODEL.CHROMA.DEPTHS = [1, 2, 1]
+_C.MODEL.CHROMA.CHANNELS = [32, 64, 32]
+_C.MODEL.CHROMA.CHANNEL_MULTIPLIER = 2
+_C.MODEL.CHROMA.IN_CHANNELS = 3
+_C.MODEL.CHROMA.OUT_CHANNELS = 3
+_C.MODEL.CHROMA.STEM_KERNEL_SIZE = 3
+_C.MODEL.CHROMA.BODY_KERNEL_SIZE = 3
+_C.MODEL.CHROMA.LEAF_KERNEL_SIZE = 3
 
 
 # -----------------------------------------------------------------------------
-# Train config
+# Training settings
 # -----------------------------------------------------------------------------
 _C.TRAIN = CN()
+_C.TRAIN.DEVICE = "mps"
 _C.TRAIN.BATCH_SIZE = 32
+_C.TRAIN.START_ITERATION = 0
+_C.TRAIN.NUM_ITERATIONS = 50_000
+_C.TRAIN.WARMUP_ITERATIONS = 1000
+_C.TRAIN.WEIGHT_DECAY = 0.05
+_C.TRAIN.BASE_LR = 5e-4
+_C.TRAIN.WARMUP_LR = 1e-6
+_C.TRAIN.MIN_LR = 5e-6
+_C.TRAIN.CLIP_GRAD = 5.0
+_C.TRAIN.CHECKPOINT_EVERY = 2000
+_C.TRAIN.CHECKPOINT_DIR = "checkpoints/"
+# _C.TRAIN.ACCUMULATION_STEPS = 1
+
+# Optimizer
+_C.TRAIN.OPTIMIZER = CN()
+_C.TRAIN.OPTIMIZER.NAME = "adamw"
+_C.TRAIN.OPTIMIZER.KWARGS = [ ("betas", (0.9, 0.999)), ("weight_decay", 1e-3) ]
+
+# Learning rate scheduler
+_C.TRAIN.LR_SCHEDULER = CN()
+_C.TRAIN.LR_SCHEDULER.NAME = "cosine"
+_C.TRAIN.LR_SCHEDULER.KWARGS = [("T_0", 10_000), ("eta_min", 1e-6), ("T_mult", 3)]
+_C.TRAIN.LR_SCHEDULER.WARMUP_PREFIX = True
 
 
 # -----------------------------------------------------------------------------
@@ -93,5 +125,45 @@ _C.TEST = CN()
 _C.TEST.BATCH_SIZE = 64
 
 
+# -----------------------------------------------------------------------------
+# Loss config
+# -----------------------------------------------------------------------------
+_C.LOSS = CN()
+_C.LOSS.CRITERION = "huber"
+_C.LOSS.CRITERION_KWARGS = [("delta", 1.0)]
+_C.LOSS.LUMA_WEIGHT = 0.5
+_C.LOSS.CHROMA_WEIGHT = 0.25
+_C.LOSS.GAMMA = 0.8
+
+# -----------------------------------------------------------------------------
+# Logging setting
+# -----------------------------------------------------------------------------
+_C.LOGGING = CN()
+_C.LOGGING.LOG_EVERY = 100
+_C.LOGGING.DIR = "checkpoints/"
+
+
+
 def default_config():
     return _C.clone()
+
+
+def get_config(filename):
+    cfg = default_config()
+    cfg.merge_from_file(filename)
+    return cfg
+
+
+def update_config_from_file(config, cfg_file):
+    config.defrost()
+    with open(cfg_file, 'r') as f:
+        yaml_cfg = yaml.load(f, Loader=yaml.FullLoader)
+
+    for cfg in yaml_cfg.setdefault('BASE', ['']):
+        if cfg:
+            update_config_from_file(
+                config, os.path.join(os.path.dirname(cfg_file), cfg)
+            )
+    print('=> merge config from {}'.format(cfg_file))
+    config.merge_from_file(cfg_file)
+    config.freeze()
