65074b0dbe5395c4a4c64cc4b4baa50403a0474d
xeparc <xeparc@gmail.com>
2024-08-25 01:47:44 +0300

    Updated train script

--- train.py
+++ train.py
@@ -1,24 +1,28 @@
+import argparse
 import datetime
 import os
 import time
 from typing import List, Callable
+import yaml
 
 from yacs.config import CfgNode
 
 from config import default_config, get_config
 from builders import *
-from utils import RunningStats
+from utils import RunningStats, save_checkpoint, load_checkpoint
 from jpegutils import SUBSAMPLE_FACTORS
 
 
 def main(cfg):
 
+    device = torch.device(cfg.TRAIN.DEVICE)
+
     train_loader    = build_dataloader(cfg, "train")
     val_loader      = build_dataloader(cfg, "val")
     test_loader     = build_dataloader(cfg, "test")
 
-    spectre         = build_spectral_net(cfg)
-    chroma          = build_chroma_net(cfg)
+    spectre         = build_spectral_net(cfg).to(device)
+    chroma          = build_chroma_net(cfg).to(device)
     criterion       = build_criterion(cfg)
 
     optim           = build_optimizer(cfg, spectre.parameters(), chroma.parameters())
@@ -31,6 +35,7 @@ def main(cfg):
 
     train_loop(cfg, spectre, chroma, criterion, train_loader, optim,
                lr_scheduler, logger)
+
 
 
 def predict_train(spectral_net, chroma_net, datapoint, subsample: str):
@@ -72,12 +77,12 @@ def train_loop(
         lr_scheduler,
         logger
     ):
-    
+
     spectral_net.train()
     chroma_net.train()
     optimizer.zero_grad()
 
-    rgbout = config.MODEL.OUTPUT_RGB
+    rgbout = config.MODEL.RGB_OUTPUT
     clip_grad = config.TRAIN.CLIP_GRAD > 0.0
 
     max_iters = config.TRAIN.NUM_ITERATIONS
@@ -139,36 +144,54 @@ def train_loop(
             total_loss_meter.update(loss["total"].item())
             spectral_loss_meter.update(loss["spectral"].item())
             chroma_loss_meter.update(loss["chroma"].item())
-            spectral_grad_norm_meter.update(spectral_grad_norm)
-            chroma_grad_norm_meter.update(chroma_grad_norm)
+            spectral_grad_norm_meter.update(spectral_grad_norm.cpu().numpy())
+            chroma_grad_norm_meter.update(chroma_grad_norm.cpu().numpy())
 
             # Checkpoint models
-            if current_iter % config.LOGGING.CHECKPOINT_EVERY:
-                pass
-
+            if current_iter % config.TRAIN.CHECKPOINT_EVERY == 0:
+                savestate = {
+                    "spectral": spectral_net.state_dict(),
+                    "chroma": chroma_net.state_dict(),
+                    "optimizer": optimizer.state_dict(),
+                    "lr_scheduler": lr_scheduler.state_dict(),
+                    "psnr": -1
+                }
+                save_checkpoint(savestate, config, current_iter, logger)
 
             # Log
-            if current_iter % config.LOGGING.LOG_EVERY:
-                lr = optimizer.param_groups[0]["lr"]
+            if current_iter % config.LOGGING.LOG_EVERY == 0:
+                lr = lr_scheduler.get_last_lr()[0]
                 wd = optimizer.param_groups[0]["weight_decay"]
                 memory_used = get_alloc_memory(config)
                 eta = batch_time_meter.mean * (max_iters - current_iter)
-                
+
                 logger.info(
-                    f"Train: [{current_iter}/{max_iters}]\t"
+                    f"Train: [{current_iter:>6}/{max_iters:>6}]\t"
                     f"eta: {datetime.timedelta(seconds=int(eta))}\t"
                     f"lr: {lr:.6f}\t wd {wd:.4f}\t"
-                    f"time: {batch_time_meter.mean:.4f} ± ({batch_time_meter.std:.4f})\t"
-                    f"spectral loss: {spectral_loss_meter.mean:.4f} ± ({spectral_loss_meter.std:.4f})\t"
-                    f"chroma loss: {chroma_loss_meter.mean:.4f} ± ({chroma_loss_meter.std:.4f})\t"
-                    f"spectral grad norm: {spectral_grad_norm_meter.mean:.4f} ± ({spectral_grad_norm_meter.std:.4f})\t"
-                    f"chroma grad norm: {chroma_grad_norm_meter.mean:.4f} ± ({chroma_grad_norm_meter.std:.4f})\t"
+                    f"time: {batch_time_meter.mean:.4f} ± {batch_time_meter.std:.4f}\t"
+                    f"spectral loss: {spectral_loss_meter.mean:.4f} ± {spectral_loss_meter.std:.4f}\t"
+                    f"chroma loss: {chroma_loss_meter.mean:.4f} ± {chroma_loss_meter.std:.4f}\t"
+                    f"total loss: {total_loss_meter.mean:.4f} ± {total_loss_meter.std:.4f}\t"
+                    f"spectral grad norm: {spectral_grad_norm_meter.mean:.4f} ± {spectral_grad_norm_meter.std:.4f}\t"
+                    f"chroma grad norm: {chroma_grad_norm_meter.mean:.4f} ± {chroma_grad_norm_meter.std:.4f}\t"
                     f"memory: {memory_used:.0f}MB"
                 )
+
+                # Reset meters
+                batch_time_meter.reset()
+                total_loss_meter.reset()
+                spectral_loss_meter.reset()
+                chroma_loss_meter.reset()
+                spectral_grad_norm_meter.reset()
+                chroma_grad_norm_meter.reset()
 
         if current_iter >= max_iters:
             break
 
+
+def validate():
+    pass
 
 
 def get_alloc_memory(config):
@@ -183,5 +206,15 @@ def get_alloc_memory(config):
 
 if __name__ == "__main__":
 
-    cfg = get_config("configs/testconfig.yaml")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", "-c", type=str)
+    args = parser.parse_args()
+
+    cfg = get_config(args.config)
+    cfg_str = cfg.dump()
+    cfg_savepath = os.path.join(cfg.LOGGING.DIR, cfg.TAG, "config.yaml")
+    os.makedirs(os.path.dirname(cfg_savepath), exist_ok=True)
+    with open(cfg_savepath, mode="w") as f:
+        cfg.dump(stream=f)
+
     main(cfg)
